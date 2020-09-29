e8596053c120565d3401366a515b3c125212d5c3
Chew <inferno.chew@gmail.com>
2020-09-29 15:17:22 +0900

    new project

---
+++ Assets/Script/Chew/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
