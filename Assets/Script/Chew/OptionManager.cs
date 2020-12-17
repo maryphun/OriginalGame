c7ab91b01e08b63a14a85eab7867aadff6b113e6
Chew <inferno.chew@gmail.com>
2020-12-17 13:36:25 +0900

    option

--- Assets/Script/Chew/OptionManager.cs
+++ Assets/Script/Chew/OptionManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 
+enum WindowMode
+{
+    Fullscreen,
+    BorderlessFullscreen,
+    Windowed
+};
+
 public class OptionManager : MonoBehaviour
 {
     private List<GameObject> pauseObjects;
@@ -13,12 +20,16 @@ public class OptionManager : MonoBehaviour
     public Slider soundVolume;
     public Slider musicVolume;
     public TMPro.TMP_Dropdown screenResolution;
-
+    public TMPro.TMP_Dropdown windowMode;
+    //public bool applyChange;
+    
     int[] horizontalResolution = new int[] { 1280, 1600, 1920 };
 
     private Resolution resolution;
-    private List<Vector2> resolutionList;
+    private List<Vector2Int> resolutionList;
     private List<string> resolutionString;
+
+    private FullScreenMode currentWindowMode;
 
     private void Awake()
     {
@@ -49,14 +60,14 @@ public class OptionManager : MonoBehaviour
         musicVolume.value = PlayerPrefs.GetFloat("MusicVolume", 1);
         SetMusicVolume(musicVolume.value);
 
-        resolutionList = new List<Vector2>();
+        resolutionList = new List<Vector2Int>();
         resolutionString = new List<string>();
         foreach (int reso in horizontalResolution)
         {
             //4:3 aspect ratio
-            resolutionList.Add(new Vector2(reso, reso / 4 * 3));
+            resolutionList.Add(new Vector2Int(reso, reso / 4 * 3));
             //16:9 aspect ratio
-            resolutionList.Add(new Vector2(reso, reso / 16 * 9));
+            resolutionList.Add(new Vector2Int(reso, reso / 16 * 9));
         }
         resolutionList = resolutionList.OrderBy(val => val.x * val.y).ToList();
         foreach (Vector2 reso in resolutionList)
@@ -67,7 +78,9 @@ public class OptionManager : MonoBehaviour
         screenResolution.AddOptions(resolutionString);
         //default resolution as 1920x1080
         screenResolution.value = PlayerPrefs.GetInt("CurrentResolution", resolutionList.FindIndex(val => val.x == 1920 && val.y == 1080));
-
+        windowMode.value = PlayerPrefs.GetInt("WindowMode", (int)WindowMode.Fullscreen);
+        SetWindowMode(windowMode.value);
+        Screen.SetResolution(resolutionList[screenResolution.value].x, resolutionList[screenResolution.value].y, currentWindowMode);
     }
 
     public void OpenMenu()
@@ -120,4 +133,46 @@ public class OptionManager : MonoBehaviour
         PlayerPrefs.SetFloat("MusicVolume", value);
 
     }
+
+    public void SetWindowMode(int value)
+    {
+        switch(value)
+        {
+            case (int)WindowMode.Fullscreen:
+                currentWindowMode = FullScreenMode.ExclusiveFullScreen;
+                break;
+            case (int)WindowMode.BorderlessFullscreen:
+                currentWindowMode = FullScreenMode.FullScreenWindow;
+                break;
+            case (int)WindowMode.Windowed:
+                currentWindowMode = FullScreenMode.Windowed;
+                break;
+        }
+        SetResolution();
+
+    }
+
+    public void SetResolution()
+    {
+        //var oldResolution = Screen.currentResolution;
+        Screen.SetResolution(resolutionList[screenResolution.value].x, resolutionList[screenResolution.value].y, currentWindowMode);
+        StartCoroutine(ChangeResolution());
+        //StartCoroutine(ApplyResolution(applyChange,oldResolution));
+
+    }
+
+    private IEnumerator ChangeResolution()
+    {
+        yield return new WaitUntil(() => Screen.width == resolutionList[screenResolution.value].x && Screen.height == resolutionList[screenResolution.value].y);
+    }
+     
+//    private IEnumerator ApplyResolution(bool doApply,Resolution revertVal)
+//    {
+//        yield return new WaitForSeconds(15);
+//        if (!doApply)
+//        {
+//            Screen.SetResolution(revertVal.width,revertVal.height, currentWindowMode);
+//            StartCoroutine(ChangeResolution());
+//        }
+//    }
 }
